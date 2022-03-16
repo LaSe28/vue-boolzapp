59586c6728c70b62a0685df8a47571c36ae452f7
LaSe28 <mattia.las28@gmail.com>
2022-03-16 19:04:12 +0100

    dont know

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ let app = new Vue({
         {
             text : '',
             sent : true,
+            clicked : false,
+
         },
         chats : [
             {
@@ -17,22 +19,27 @@ let app = new Vue({
                     {
                         text : 'ciao',
                         sent : false,
+                        clicked : false,
                     },
                     {
                         text : 'hai la macchina stasera?',
                         sent : false,
+                        clicked : false,
                     },
                     {
                         text : 'ti serve un passaggio?',
                         sent : true,
+                        clicked : false,
                     },
                     {
                         text : 'si',
                         sent : false,
+                        clicked : false,
                     },
                     {
                         text : 'allora no',
                         sent : true,
+                        clicked : false,
                     }
                 ],
 
@@ -46,10 +53,12 @@ let app = new Vue({
                     {
                         text : 'Teleciamaragno chiama ragno, ragno rispondi',
                         sent : true,
+                        clicked : false,
                     },
                     {
                         text : 'sei ubriaco?',
                         sent : false,
+                        clicked : false,
                     }
                 ],
 
@@ -63,10 +72,12 @@ let app = new Vue({
                     {
                         text : 'ei',
                         sent : true,
+                        clicked : false,
                     },
                     {
                         text : 'ciao dimmi',
                         sent : false,
+                        clicked : false,
                     }
                 ],
 
@@ -80,10 +91,12 @@ let app = new Vue({
                     {
                         text : 'tutto bene?',
                         sent : true,
+                        clicked : false,
                     },
                     {
                         text : 'insomma',
                         sent : false,
+                        clicked : false,
                     }
                 ],
 
@@ -97,18 +110,22 @@ let app = new Vue({
                     {
                         text : 'ciao',
                         sent : true,
+                        clicked : false,
                     },
                     {
                         text : 'come stai?',
                         sent : false,
+                        clicked : false,
                     },
                     {
                         text : 'abbastanza bene dai, tu?',
                         sent : true,
+                        clicked : false,
                     },
                     {
                         text : 'anche io grazie',
                         sent : false,
+                        clicked : false,
                     },
                 ],
 
@@ -122,10 +139,12 @@ let app = new Vue({
                     {
                         text : 'faccio un po di ritardo a lavoro',
                         sent : false,
+                        clicked : false,
                     },
                     {
                         text : 'strano',
                         sent : true,
+                        clicked : false,
                     }
                 ],
 
@@ -133,37 +152,43 @@ let app = new Vue({
             {
                 contact : {
                     name : '+39 3985258932',
-                    img: 'avatar_7.jpg',
+                    img: 'avatar_io.jpg',
                 },
                 messages : [
                     {
                         text : 'hola',
                         sent : false,
+                        clicked : false,
                     },
                     {
                         text : 'amigo',
                         sent : false,
+                        clicked : false,
                     },
                     {
                         text: 'chi sei?',
-                        sent: true
+                        sent: true,
+                        clicked : false,
                     }
                 ],
+                
 
             },
             {
                 contact : {
                     name : 'Gregorio',
-                    img: 'avatar_io.jpg',
+                    img: 'avatar_7.jpg',
                 },
                 messages : [
                     {
                         text : 'ciao',
                         sent : true,
+                        clicked : false,
                     },
                     {
                         text : 'ehila',
                         sent : false,
+                        clicked : false,
                     }
                 ],
 
@@ -175,8 +200,11 @@ let app = new Vue({
         addMessage(){
             if(this.newMessage != ''){
                 this.chats[this.i].messages.push(this.newMessage)
-                setTimeout(() => {this.chats[this.i].messages.push({text : 'ok',sent : false,})}, 1000);}
-            this.newMessage = {text : '',sent : true}
+                setTimeout(() => {this.chats[this.i].messages.push({text : 'ok',sent : false,hour: luxon.DateTime.now().toFormat('dd/MM/yyyy HH:mm')})}, 1000);}
+            this.newMessage = {text : '',sent : true,hour: luxon.DateTime.now().toFormat('dd/MM/yyyy HH:mm')}
+        },
+        prova(index){
+            console.log(index, 'ciao', this.chats[index].contact.name)
         }
     }
 })
